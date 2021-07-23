12d2436d702bbd469902938c356cb341b7188359
James L <59751464+Jazz1016@users.noreply.github.com>
2021-07-23 13:44:01 -0600

    got mp4 background playing but not repeating

--- crystalBallProgrammatic/crystalBallProgrammatic/Controllers/ViewControllers/CrystallBallViewController.swift
+++ crystalBallProgrammatic/crystalBallProgrammatic/Controllers/ViewControllers/CrystallBallViewController.swift
@@ -6,21 +6,23 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class CrystalBallViewController: UIViewController {
-    
     // MARK: - Lifecycle
     override func loadView() {
         super.loadView()
         
-        self.view.addSubview(titleLabel)
-        self.view.addSubview(imageView)
+        addSubviews()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
         constrainAll()
+        playVideo()
     }
     
     // MARK: - Properties
@@ -31,9 +33,10 @@ class CrystalBallViewController: UIViewController {
     // MARK: - Functions
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "James is the best"
+        label.text = "Ask a question and shake the crystal ball to get a response"
         label.textAlignment = .center
         label.backgroundColor = .green
+        label.numberOfLines = 0
         
         return label
     }()
@@ -45,12 +48,57 @@ class CrystalBallViewController: UIViewController {
         return imageView
     }()
     
-    func constrainAll(){
-        titleLabel.anchor(top: self.safeArea.topAnchor, bottom: self.safeArea.bottomAnchor, leading: titleLabel.leadingAnchor, trailing: titleLabel.trailingAnchor, paddingTop: 16, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 200, height: 32)
+    let safeAreaView: UIView = {
+        let view = UIView()
         
+        return view
+    }()
+    
+    func playVideo(){
         
+        guard let path = Bundle.main.path(forResource: "stars", ofType: "mp4") else {return}
+        let videoAsset = AVURLAsset(url: URL(fileURLWithPath: path))
+        let playerItem = AVPlayerItem(asset: videoAsset)
+        
+//        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let player = AVQueuePlayer(playerItem: playerItem)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        safeAreaView.layer.addSublayer(playerLayer)
+        player.volume = 0
+        
+        DispatchQueue.main.async {
+            let loopItem = AVPlayerItem(asset: videoAsset)
+            player.insert(loopItem, after: nil)
+            
+            player.play()
+        }
     }
     
+    func addSubviews(){
+        self.view.addSubview(safeAreaView)
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(imageView)
+    }
     
+    func constrainAll(){
+        safeAreaView.anchor(top: self.safeArea.topAnchor, bottom: self.safeArea.bottomAnchor, leading: self.safeArea.leadingAnchor, trailing: self.safeArea.trailingAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
+        
+        imageView.frame = CGRect(x: self.view.frame.size.width/2 - 200, y: 96, width: 400, height: 400)
+        
+        titleLabel.frame = CGRect(x: self.view.frame.size.width/2 - 100, y: imageView.frame.maxY + 16, width: 200, height: 64)
+        
+//        titleLabel.anchor(top: imageView.bottomAnchor, bottom: nil, leading: titleLabel.leadingAnchor, trailing: titleLabel.trailingAnchor, paddingTop: 16, paddingBottom: 0, paddingLeft: 0, paddingRight: 0)
+    }
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        
+        return stackView
+    }()
     
 }//End of class
